d900ebac83dce13d95cb9a69053b46af91e15936
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-07-27 00:38:26 -0300

    installing firebase and angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB29bYmAyhC-T8ziJX8dXZ9uCVSta_zUWQ",
+    authDomain: "oshop-f7da8.firebaseapp.com",
+    databaseURL: "https://oshop-f7da8-default-rtdb.firebaseio.com/",
+    projectId: "oshop-f7da8",
+    storageBucket: "oshop-f7da8.appspot.com",
+    messagingSenderId: "111258716050",
+    appId: "1:111258716050:web:21b5e030c36ac122c6234b",
+    measurementId: "G-DJDPQ9956P"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB29bYmAyhC-T8ziJX8dXZ9uCVSta_zUWQ",
+    authDomain: "oshop-f7da8.firebaseapp.com",
+    databaseURL: "https://oshop-f7da8-default-rtdb.firebaseio.com/",
+    projectId: "oshop-f7da8",
+    storageBucket: "oshop-f7da8.appspot.com",
+    messagingSenderId: "111258716050",
+    appId: "1:111258716050:web:21b5e030c36ac122c6234b",
+    measurementId: "G-DJDPQ9956P"
+  }
 };
 
 /*
