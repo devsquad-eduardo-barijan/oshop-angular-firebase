d4cf1486e12d4a3a7310fc0637e1364195509bf4
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-07-28 23:56:14 -0300

    save user to firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyB29bYmAyhC-T8ziJX8dXZ9uCVSta_zUWQ",
     authDomain: "oshop-f7da8.firebaseapp.com",
-    databaseURL: "https://oshop-f7da8-default-rtdb.firebaseio.com/",
+    databaseURL: "https://oshop-f7da8-default-rtdb.firebaseio.com",
     projectId: "oshop-f7da8",
     storageBucket: "oshop-f7da8.appspot.com",
     messagingSenderId: "111258716050",
