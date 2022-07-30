a117c2779dafe298e783b89615e2f70f5821b0fb
Eduardo Barijan <eduardo.barijan@devsquad.com>
2022-07-29 23:48:37 -0300

    protect admin routes

---
+++ src/app/admin-auth-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private userService: UserService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.auth.user$.pipe(
+      switchMap(user => this.userService.get(user.uid).valueChanges()),
+      map(appUser => appUser.isAdmin)
+    );
+  }
+}
